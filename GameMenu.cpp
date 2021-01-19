fdbcedac13e852bd5194109670f3708695f46bea
Cesar <cesar.lortet@ynov.com>
2021-01-19 11:05:10 +0100

    game menu + ui

--- GameMenu.cpp
+++ GameMenu.cpp
@@ -3,6 +3,7 @@
 GameMenu *GameMenu::m_gameMenu = nullptr;
 
 GameMenu::GameMenu() {
+	m_currIndex = 1;
 	drawMenu(DEF_MAIN_MENU);
 }
 
@@ -19,39 +20,98 @@ GameMenu *GameMenu::getInstance() {
 
 void GameMenu::drawMenu(int menuIndex) {
 	system("cls");
-	for (int i = 0; i < m_menus.at(menuIndex).size(); i++) {
-		i == 0 ? std::cout << " > " : std::cout << i << " - ";
+	std::ifstream title;
+	title.open("ui/title.ui", std::ios::in);
+	if (title.is_open())
+		std::cout << title.rdbuf() << std::endl;
+	
+	std::ifstream menu;
+	std::string menuUrl = "ui/" + m_menus.at(menuIndex).at(0) + ".ui";
+	menu.open(menuUrl, std::ios::in);
+	if (menu.is_open())
+		std::cout << menu.rdbuf() << std::endl;
+	std::cout << "\n";
+
+	for (int i = 1; i < m_menus.at(menuIndex).size(); i++) {
+		std::cout << "\t\t\t";
+		i == m_currIndex ? std::cout << DEF_SELECT_CHAR << " " : std::cout << "  ";
 		std::cout << m_menus.at(menuIndex).at(i) << std::endl;
 	}
+	title.close();
+	menu.close();
+	selectAction(menuIndex);
+}
 
-	std::cout << "Action : ";
-	std::cin >> m_choice;
-	m_index = std::stoi(m_choice);
+void GameMenu::startNewGame(int nbrOfPlayers) {
+	GameManager::getInstance()->initNewGame(m_numberOfPlayers);
+
+}
+
+void GameMenu::selectAction(int menuIndex)
+{
+	char key_press;
+	int ascii_value;
+	while (1)
+	{
+		key_press = _getch();
+		ascii_value = key_press;
+		int maxIndex = m_menus.at(menuIndex).size() - 1;
+		if (ascii_value == 80) {
+			m_currIndex++;
+			if (m_currIndex > maxIndex) {
+				m_currIndex = 1;
+			}
+			drawMenu(menuIndex);
+			break;
+		}
+		else if (ascii_value == 72) {
+			m_currIndex--;
+			if (m_currIndex < 1) {
+				m_currIndex = maxIndex;
+			}
+			drawMenu(menuIndex);
+			break;
+		}
+		else if (ascii_value == 13) {
+			menuSelected(menuIndex, m_currIndex);
+			break;
+		}
+	}
+}
+
+void GameMenu::menuSelected(int menuIndex, int index) {
+
+	m_index = index;
 
 	if (m_index < m_menus.at(menuIndex).size() && m_index > 0) {
 		switch (menuIndex) {
 		case DEF_MAIN_MENU:
-			m_index == 1 ? startNewGame() : drawMenu(DEF_EXIT_CONFIRM);
+			m_index == 1 ? drawMenu(DEF_HOW_MANY) : drawMenu(DEF_EXIT_CONFIRM);
 			break;
 		case DEF_EXIT_CONFIRM:
 			m_index == 1 ? exit(EXIT_SUCCESS) : drawMenu(DEF_MAIN_MENU);
 			break;
+		case DEF_HOW_MANY:
+			switch (m_index)
+			{
+			case 1:
+				startNewGame(2);
+				break;
+			case 2:
+				startNewGame(3);
+				break;
+			case 3:
+				startNewGame(4);
+				break;
+			case 4:
+				drawMenu(DEF_MAIN_MENU);
+				break;
+			}
 		}
 	}
 	else {
 		std::cout << "Unkonwn Menu, returning to Main Menu.\n";
 		drawMenu(DEF_MAIN_MENU);
 	}
-}
-
-void GameMenu::startNewGame() {
-	std::cout << std::endl << "Enter the number of players : ";
-	std::cin >> m_numberOfPlayers;
-	if (m_numberOfPlayers < 2) {
-		std::cout << "\nAt least 2 players are needed !\n";
-		startNewGame();
-	}
-	GameManager::getInstance()->initNewGame(m_numberOfPlayers);
-
 }
 
--- GameMenu.h
+++ GameMenu.h
@@ -4,9 +4,14 @@
 
 #define DEF_MAIN_MENU		0
 #define DEF_EXIT_CONFIRM	1
-#define DEF_NEW_GAME		2
+#define DEF_HOW_MANY		2
+
+#define DEF_SELECT_CHAR		"»"
 
 #include "GameManager.h"
+#include <iostream>
+#include <fstream>
+#include<conio.h>
 
 
 class GameMenu
@@ -15,16 +20,22 @@ class GameMenu
 		~GameMenu();
 		static GameMenu* getInstance();
 		void drawMenu(int menuIndex);
+		void menuSelected(int menuIndex, int index);
+		void selectAction(int menuIndex);
+		void startNewGame(int nbrOfPlayers);
+
+	private:
 		std::string m_choice;
 		int m_index;
 		int m_numberOfPlayers;
-
-	private:
-		GameMenu();
-		void startNewGame();
+		int m_currIndex;
 		static GameMenu* m_gameMenu;
-		std::vector<std::vector<std::string>> m_menus = { {"Main Menu", "New Game", "Exit"},
-														{"Sure ?", "Yes", "No"}};
+		std::vector<std::vector<std::string>> m_menus = { 
+															{ "mainmenu", "New Game", "Exit" },
+															{ "sure", "Yes", "No" },
+															{ "selectplayers", "2 players", "3 players", "4 players", "Back" }
+														};
+		GameMenu();
 };
 
 #endif
